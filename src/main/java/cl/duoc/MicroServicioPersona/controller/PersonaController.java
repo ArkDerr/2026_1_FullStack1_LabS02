9495e073aad2d1ead9b7560a6566aa202f727ac4
unknown <Cetecom@SJAPCA03LC0401.duoc.acad>
2026-03-25 20:29:39 -0300

    Se agrega validaciones y codigos

--- src/main/java/cl/duoc/MicroServicioPersona/controller/PersonaController.java
+++ src/main/java/cl/duoc/MicroServicioPersona/controller/PersonaController.java
@@ -3,6 +3,7 @@ package cl.duoc.MicroServicioPersona.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -15,6 +16,7 @@ import org.springframework.web.bind.annotation.RestController;
 import cl.duoc.MicroServicioPersona.model.Persona;
 import cl.duoc.MicroServicioPersona.model.PersonaUpdate;
 import cl.duoc.MicroServicioPersona.service.PersonaService;
+import jakarta.validation.Valid;
 
 @RestController
 @RequestMapping("/api/v1/personas")
@@ -37,8 +39,13 @@ public class PersonaController {
 
     // POST para guardar persona
     @PostMapping
-    public Persona guardarPersona(@RequestBody Persona persona) {
-        return personaService.guardarPersona(persona);
+    public ResponseEntity<?> guardarPersona(@Valid @RequestBody Persona persona) {
+        try {
+            Persona nuevaPersona = personaService.guardarPersona(persona);
+            return ResponseEntity.status(201).body(nuevaPersona);
+        } catch (Exception e) {
+            return ResponseEntity.status(400).body("El usuario no puede ser agregado");
+        }
     }
 
     // PUT para actualizar persona
--- src/main/java/cl/duoc/MicroServicioPersona/model/Persona.java
+++ src/main/java/cl/duoc/MicroServicioPersona/model/Persona.java
@@ -1,5 +1,8 @@
 package cl.duoc.MicroServicioPersona.model;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -8,7 +11,13 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 public class Persona {
+
+    @NotNull(message = "El campo es obligatorio")
     private int rut;
+
+    //@Positive(message = "El valor debe ser mayor a cero")
     private char dv;
+
+    @NotBlank(message = "El campo es obligatorio")
     private String nombre;
 }
